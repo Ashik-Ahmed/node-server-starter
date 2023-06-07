7c96b3dd9fbe6e4b3d6ba4d403b5531a8646b20d
unknown <ashikahmed121@gmail.com>
2023-06-07 12:45:48 +0600

    node server starter project

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+
+//middleware
+app.use(express.json());
+app.use(cors());
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/', (req, res) => {
+    res.send('Server is Running!!')
+    console.log('api hit');
+})
+
+
+module.exports = app;
